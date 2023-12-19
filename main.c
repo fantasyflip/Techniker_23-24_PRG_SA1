c00982194b2c6763a6385b65ab1fc88da83bdd40
Philipp Katzenberger <katzenberger.ph+github@gmail.com>
2023-12-19 22:51:13 +0100

    feat(main): variant 2 solution for task 2

--- main.c
+++ main.c
@@ -36,60 +36,129 @@ void task2variant1(){
 
     printf("Rueckgabe: %.2f\n\n", moneyReturn);
 
-    if(moneyReturn > 2.0){
+    if(moneyReturn >= 2.0){
         for(int i = 0; moneyReturn > 2.0; i ++){
             euro2++;
             moneyReturn = moneyReturn - 2.0;
         }
     }
 
-    if(moneyReturn > 1.0){
+    if(moneyReturn >= 1.0){
         for(int i = 0; moneyReturn > 1.0; i ++){
             euro1++;
             moneyReturn = moneyReturn - 1.0;
         }
     }
 
-    if(moneyReturn > 0.5){
+    if(moneyReturn >= 0.5){
         for(int i = 0; moneyReturn > 0.5; i ++){
             cent50++;
             moneyReturn = moneyReturn - 0.5;
         }
     }
 
-    if(moneyReturn > 0.2){
+    if(moneyReturn >= 0.2){
         for(int i = 0; moneyReturn > 0.2; i ++){
             cent20++;
             moneyReturn = moneyReturn - 0.2;
         }
     }
 
-    if(moneyReturn > 0.1){
+    if(moneyReturn >= 0.1){
         for(int i = 0; moneyReturn > 0.1; i ++){
             cent10++;
             moneyReturn = moneyReturn - 0.1;
         }
     }
 
-    if(moneyReturn > 0.05){
+    if(moneyReturn >= 0.05){
         for(int i = 0; moneyReturn > 0.05; i ++){
             cent5++;
             moneyReturn = moneyReturn - 0.05;
         }
     }
 
-    if(moneyReturn > 0.02){
+    if(moneyReturn >= 0.02){
         for(int i = 0; moneyReturn > 0.02; i ++){
             cent2++;
             moneyReturn = moneyReturn - 0.02;
         }
     }
 
-    if(moneyReturn > 0.01){
+    if(moneyReturn >= 0.01){
         for(int i = 0; moneyReturn > 0.01; i ++){
             cent1++;
             moneyReturn = moneyReturn - 0.01;
         }
+    }
+
+    printf("Muenzenausgabe:\n\n");
+
+    printf("2  Euro:\t%d\n", euro2);
+    printf("1  Euro:\t%d\n", euro1);
+    printf("50 Cent:\t%d\n", cent50);
+    printf("20 Cent:\t%d\n", cent20);
+    printf("10 Cent:\t%d\n", cent10);
+    printf("5  Cent:\t%d\n", cent5);
+    printf("2  Cent:\t%d\n", cent2);
+    printf("1  Cent:\t%d\n", cent1);
+}
+
+void task2variant2(){
+    float moneyReturn = 5.49;
+    int centReturn = (int)(moneyReturn * 100);
+
+    int euro2 = 0;
+    int euro1 = 0;
+    int cent50 = 0;
+    int cent20 = 0;
+    int cent10 = 0;
+    int cent5 = 0;
+    int cent2 = 0;
+    int cent1 = 0;
+
+    printf("Rueckgabe: %.2f\n\n", moneyReturn);
+
+
+
+    while(centReturn >= 200){
+        euro2++;
+        centReturn = centReturn -200;
+    }
+
+    while(centReturn >= 100){
+        euro1++;
+        centReturn = centReturn -100;
+    }
+
+    while(centReturn >= 50){
+        cent50++;
+        centReturn = centReturn -50;
+    }
+
+    while(centReturn >= 20){
+        cent20++;
+        centReturn = centReturn -20;
+    }
+
+    while(centReturn >= 10){
+        cent10++;
+        centReturn = centReturn -10;
+    }
+
+    while(centReturn >= 5){
+        cent5++;
+        centReturn = centReturn -5;
+    }
+
+    while(centReturn >= 2){
+        cent2++;
+        centReturn = centReturn -2;
+    }
+
+    while(centReturn >= 1){
+        cent1++;
+        centReturn = centReturn -1;
     }
 
     printf("Muenzenausgabe:\n\n");
@@ -109,6 +178,8 @@ int main()
     task1();
     printf("\n\n\n");
     task2variant1();
+    printf("\n\n\n");
+    task2variant2();
     printf("\n\n\n");
     return 0;
 }
