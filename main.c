1e90f14d3980d42f0ab706fdac236e9db7d6ebd2
Philipp Katzenberger <katzenberger.ph+github@gmail.com>
2023-12-19 22:26:34 +0100

    feat(main): solution for task 1

--- main.c
+++ main.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
 
+void task1(){
+#define YEARS 10
+    float lastVal = 100000.0;
+    float percentage = 0.04;
+    float totalInvestReturn = 0.0;
+
+    printf("+-----------------------------+\n");
+    printf("| Jahr    Gewinn       Zinsen |\n");
+    printf("+-----------------------------+\n");
+    printf("| %2d  %10.2f    %9.2f |\n", 0, lastVal, totalInvestReturn);
+
+    for(int i = 1; i <= YEARS; i++){
+        float investReturn = lastVal * percentage;
+        totalInvestReturn = totalInvestReturn + investReturn;
+        printf("| %2d  %10.2f    %9.2f |\n", i, lastVal+investReturn, totalInvestReturn);
+
+        lastVal = lastVal + investReturn;
+    }
+
+    printf("+-----------------------------+\n");
+}
+
 int main()
 {
-    printf("Hello World!\n");
+    task1();
+    printf("\n\n\n");
     return 0;
 }
